60fe04b2d2a7a4475e9372c2da7a39815a6f2427
tomdepplito <tomdepplito@gmail.com>
2012-12-02 17:12:44 -0800

    Modified factories with sequences

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,42 +1,47 @@
-require 'spec_helper'
+FactoryGirl.define do
+  sequence(:email) { |n| "person#{n}@example.com" }
+  sequence(:name) { |n| "Name #{n}" }
+  sequence(:id) { |n| "#{n}"*3 }
+  sequence(:string) { |n| ('a'..'z').to_a.shuffle[0,8].join }
+end
 
 FactoryGirl.define do
   factory :user do
-    email generate(:email)
-    encrypted_password "$2a$10$2zUBqYbapGwjfA/x3O92HOk673Ai.uIabW1dvYqnMaLWyWGSOoEyi"
+    email { FactoryGirl.generate(:email) }
+    password { FactoryGirl.generate(:string) }
   end
 end
 
 FactoryGirl.define do
   factory :company do
-    name 'My Company'
-    admin_id 123456789
+    name { FactoryGirl.generate(:name) }
+    admin_id { FactoryGirl.generate(:id) }
   end
 end
 
 FactoryGirl.define do
   factory :conversation do
-    sender_id 123456789
-    receiver_id 987654321
-    body "hello"
+    sender_id { FactoryGirl.generate(:id) }
+    receiver_id { FactoryGirl.generate(:id) }
+    body { FactoryGirl.generate(:string) }
     parent_id
   end
 end
 
 FactoryGirl.define do
   factory :office do
-    street "Woodmere Dr."
+    street { FactoryGirl.generate(:string) }
     house_num 2
-    state "NY"
+    state { FactoryGirl.generate(:string) }
     zip 11768
-    phone_num "631-757-4809"
-    company_id 123456789
+    phone_num { FactoryGirl.generate(:string) }
+    company_id { FactoryGirl.generate(:id) }
   end
 end
 
 FactoryGirl.define do
   factory :employee do
-    user_id 987654321
-    company_id 123456789
+    user_id { FactoryGirl.generate(:id) }
+    company_id { FactoryGirl.generate(:id) }
   end
 end
