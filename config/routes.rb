fa07503033db80939eea0dbd80129bdda1683d1a
tomdepplito <tomdepplito@gmail.com>
2012-12-28 20:16:37 -0500

    Added some JS that will eventually be for subscription form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Otacon::Application.routes.draw do
-  devise_for :users
-  #match "/users/sign_out" => "users#index"
+  devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
   resources :companies
   resources :company do
