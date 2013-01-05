dd883e438cb9ff4669813347ff78fbcd552216aa
tomdepplito <tomdepplito@gmail.com>
2013-01-05 10:39:29 -0800

    Editing Specialty list now uses JS

--- app/assets/javascripts/edit_specialty_lists.js
+++ app/assets/javascripts/edit_specialty_lists.js
@@ -1,4 +1,4 @@
-$(".electronics").click(function() {
-  alert("worked!");
+$(document).on('click', '.electronics', function(){
+  $.ajax({url: '/specialty_lists/' + userId, data:"delete_id=" + this.id, type: "DELETE"});
+  $('#' + this.id).hide();
 });
-
--- app/controllers/specialty_lists_controller.rb
+++ app/controllers/specialty_lists_controller.rb
@@ -6,15 +6,21 @@ class SpecialtyListsController < ApplicationController
 
   def update
     @specialty_list = SpecialtyList.find_by_owner_id(current_user.id)
-    @specialty_list.update_attributes(params[:specialty_list])
+    @new_word = params[:electronics]
     @specialty_list.electronics << to_regex(params[:electronics]) unless params[:electronics].blank?
     @specialty_list.mechanical << to_regex(params[:mechanical]) unless params[:mechanical].blank?
-    if @specialty_list.save
-      flash[:success] = "List Updated!"
-      redirect_to users_path
-    else
-      flash[:error] = "Something went wrong"
-      render :edit
+    @specialty_list.save
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def destroy
+    @specialty_list = SpecialtyList.find_by_owner_id(current_user.id)
+    @specialty_list.electronics.slice!(params[:delete_id].to_i)
+    @specialty_list.save
+    respond_to do |format|
+      format.js
     end
   end
 
@@ -22,10 +28,5 @@ class SpecialtyListsController < ApplicationController
 
   def to_regex(string)
     Regexp.new(string.singularize+"s{0,1}")
-  end
-
-  def keyword_exists?(existing_words, keyword)
-    return true if existing_words.index(to_regex(keyword))
-    false
   end
 end
