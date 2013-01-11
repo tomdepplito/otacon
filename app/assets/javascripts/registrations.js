61e9e6d77baee99cd10ff564175621a30574f377
tomdepplito <tomdepplito@gmail.com>
2013-01-10 21:01:50 -0800

    Deleted registrations.js

--- app/assets/javascripts/registrations.js
+++
@@ -1,9 +0,0 @@
-$("#subscription").hide();
-
-$("input:radio[value=true]").click(function() {
-  $("#subscription").show();
-});
-
-$("input:radio[value=false]").click(function() {
-  $("#subscription").hide();
-});
