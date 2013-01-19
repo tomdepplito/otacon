3c50bb1fb7a24a7cf924d80b784d20f0c1f7242d
tomdepplito <tomdepplito@gmail.com>
2013-01-19 11:32:53 -0800

    Now showing previous messages in rfi show view

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,6 @@
 class Conversation < ActiveRecord::Base
   has_many :attachments
   attr_accessible :body, :parent_id, :sender_id, :receiver_id, :street_address, :latitude, :longitude, :subject, :attachment, :attachments_attributes
+  belongs_to :employee
+  belongs_to :user
 end
