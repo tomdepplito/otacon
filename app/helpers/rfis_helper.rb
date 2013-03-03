758b85226a1059cae9b7efced8ab3e14d49398a5
tomdepplito <tomdepplito@gmail.com>
2013-03-02 16:34:52 -0800

    Added subject helper and grid layout for my_rfis

--- app/helpers/rfis_helper.rb
+++ app/helpers/rfis_helper.rb
@@ -1,2 +1,8 @@
 module RfisHelper
+  def subject_shortner(subject)
+    length = subject.length
+    return subject if length < 80
+    index = subject[0..80].rindex(' ')
+    index ? "#{subject[0..index-1]}..." : "#{subject[0..80]}..."
+  end
 end
